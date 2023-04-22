42d26294faa36de38aaba30c6dfa7fb45b6d029f
Sumanth Tej <96919029+Sumanth-tej@users.noreply.github.com>
2023-04-22 18:41:53 +0530

    Add files via upload

---
+++ Percentage.py
@@ -0,0 +1,4 @@
+num = float(input('Enter the Number :-'))
+percentage = float(input('Enter the percentage :-'))
+result = (percentage/100)*num
+print('{}% of {} is {}' .format(num,percentage,result))
